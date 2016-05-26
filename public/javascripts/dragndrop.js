bad98d69505bcc0b8891e0bf1feeee67336d78b3
Aaron Martin <sbbeachvball@gmail.com>
2016-05-26 16:22:02 -0700

    Many changes, for the most part working

--- public/javascripts/dragndrop.js
+++ public/javascripts/dragndrop.js
@@ -2,15 +2,51 @@ function allowDrop(ev) {
     ev.preventDefault();
 }
 
+function hasClass(element, cls) {
+    return (' ' + element.className + ' ').indexOf(' ' + cls + ' ') > -1;
+}
+
 function drag(ev) {
     ev.dataTransfer.setData("text", ev.target.id);
 }
 
 function drop(ev) {
-    ev.preventDefault();
+    console.log('ELement dropped');
     var data = ev.dataTransfer.getData("text");
-    ev.target.appendChild(document.getElementById(data));
+    var droppedElement = document.getElementById(data);
+    var dropTarget = ev.target;
+    
+    if ( hasClass(dropTarget,"player") || hasClass(dropTarget,"coach") ) {
+        // drop in parent node
+        //console.log('droppedElement: '+droppedElement.id+" hasClass(player) dropTarget:"+dropTarget.id);
+        dropTarget = dropTarget.parentNode;
+    }
+    if (hasClass(dropTarget,"playerdrop")){
+        dropTarget = dropTarget.parentNode;
+    }
+    if (hasClass(dropTarget,"coachdrop")){
+        dropTarget = dropTarget.parentNode;
+    }
+    
+    // two categories we want to prevent
+    // dropping people on coachdiv and dropping coaches on players
+    if( false ){
+    }
+    else if ( dropTarget.id == "coaches" && hasClass(droppedElement,"player")) {
+        console.log('attempting to drop player on coaches');
+    }
+    else if ( hasClass(dropTarget,"players") && hasClass(droppedElement,"coach")) {
+        console.log('attempting to drop coach on players');
+    }
+    else if ( hasClass(dropTarget,"nodrop") ){
+    }
+    else {
+        console.log(dropTarget.id);
+        dropTarget.appendChild(droppedElement);
+    }
+    ev.preventDefault();
 }
+
 function checkAlert(){
     alert("hey there");
 }
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,57 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Drag and Drop Team Builder - Proof of Concept' });
+  res.render('index', { 
+    title: 'Drag and Drop Team Builder - Proof of Concept' , 
+    coaches: [
+      { id:'01', name:'Aaron Martin'},
+      { id:'02', name:'Steve Cassin'},
+      { id:'03', name:'Paula Cassin'},
+      { id:'04', name:'Julie Landis'},
+      { id:'05', name:'Carley Anderson'},
+      { id:'06', name:'Matt Earls'},
+      { id:'07', name:'Alan Watson'},
+      { id:'08',name:'Giulia Brofferio'}
+    ],
+    divisions: [ 
+      { name:'7-8', 
+        teams:[
+          {id:'01',name:'Team-01',coaches:[],players:[]},
+          {id:'02',name:'Team-02',coaches:[],players:[]},
+          {id:'03',name:'Team-03',coaches:[],players:[]}
+        ],
+        players:[ 
+          {id:'001', name: 'Joey', age: '8'},
+          {id:'002', name: 'Jill', age: '7'},
+          {id:'003', name: 'Sabrina', age: '7'}
+        ]
+      },
+      { name:'9-10', 
+        teams:[
+          {id:'04',name:'Team-04',coaches:[],players:[]},
+          {id:'05',name:'Team-05',coaches:[],players:[]},
+          {id:'06',name:'Team-06',coaches:[],players:[]}
+        ],
+        players:[ 
+          {id:'0025', name: 'Kelly', age: '9'},
+          {id:'0035', name: 'Kim', age: '10'},
+          {id:'0043', name: 'Kyle', age: '8'}
+        ]
+      },
+      { name:'11-12', 
+        teams:[
+          {id:'07',name:'Team-07',coaches:[],players:[]},
+          {id:'08',name:'Team-08',coaches:[],players:[]},
+          {id:'09',name:'Team-09',coaches:[],players:[]}
+        ],
+        players:[ 
+          {id:'101', name: 'Lola', age: '11'},
+          {id:'102', name: 'Lyle', age: '12'},
+          {id:'103', name: 'Lovitz', age: '11'}
+        ]
+      }
+    ]
+  })
 });
 
 module.exports = router;
